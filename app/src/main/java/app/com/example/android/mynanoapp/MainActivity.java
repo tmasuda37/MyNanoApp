e67cc4e0a0cef03be3e2bcdb3b24c025af9e3bd0
Toshi <toshifumi.masuda@gmail.com>
2015-09-16 23:23:30 +0100

    My App Portfolio

--- app/src/main/java/app/com/example/android/mynanoapp/MainActivity.java
+++ app/src/main/java/app/com/example/android/mynanoapp/MainActivity.java
@@ -1,20 +1,30 @@
 package app.com.example.android.mynanoapp;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MyActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate() method is called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu() method is called");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -22,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected() method is called");
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -33,5 +44,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showMessage(View aView) {
+        Log.d(TAG, "showMessage() method is called");
+
+        Button aButton = (Button) aView;
+        aButton.setBackgroundColor(Color.YELLOW);
+        Context context = getApplicationContext();
+        CharSequence text = "The button will language my " + aButton.getText() + " App!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
